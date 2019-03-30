48d5ce1e7d646b13c26f44e1d15f0cdc12d00269
Akela <akela@null.net>
2019-03-30 11:27:05 +0100

    Implemented UV Coords

--- SPICA/Formats/Generic/Blender/BLEND.cs
+++ SPICA/Formats/Generic/Blender/BLEND.cs
@@ -1,18 +1,20 @@
-﻿using SPICA.Formats.Common;
-using SPICA.Formats.CtrH3D;
+﻿using SPICA.Formats.CtrH3D;
 using SPICA.Formats.CtrH3D.Model;
+using SPICA.Formats.CtrH3D.Model.Mesh;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace SPICA.Formats.Generic.Blender
 {
 	public class BLEND
 	{
-		const float SCALE = 0.05f;
-	
-		private readonly string blenderPath = "";
-		private string pythonScript = "import bpy\nimport bmesh\n";
+		const float SCALE = 0.01f;
+
+		private readonly string blenderPath;
+		private string pythonScript;
 
 		public BLEND(H3D scene, int mdlIndex, int animIndex = -1)
 		{
@@ -31,9 +33,15 @@ namespace SPICA.Formats.Generic.Blender
 				default:
 					throw new PlatformNotSupportedException();
 			}
+
 			if (mdlIndex != -1)
 			{
 				var model = scene.Models[mdlIndex];
+
+				pythonScript = CleanUpScene(model);
+
+				if (model.Materials.Count > 0)
+					pythonScript += BuildMaterials(model);
 
 				if (model.Meshes.Count > 0)
 					pythonScript += BuildModel(model);
@@ -59,42 +67,122 @@ namespace SPICA.Formats.Generic.Blender
 				Arguments = $"--python \"{scriptPath}\""
 			};
 
-			Process.Start(startInfo);
+			Process.Start(startInfo).WaitForExit();
 		}
 
 		#region Private Methods
+		private string CleanUpScene(H3DModel model)
+		{
+			var armature = model.Skeleton.Count == 0 ? "None" : $"bpy.data.armatures.new('{model.Skeleton[0].Name}')";
+
+			return
+				"import bpy\n" +
+				"import bmesh\n" +
+				"bpy.context.scene.render.engine = 'BLENDER_RENDER'\n" +
+				"bpy.ops.object.select_all()\n" +
+				"bpy.ops.object.delete()\n" +
+				$"root = bpy.data.objects.new('{model.Name}', {armature})\n" +
+				"bpy.context.scene.objects.link(root)\n";
+		}
+
+		private string BuildMaterials(H3DModel model)
+		{
+			var res = "";
+
+			for (var mi = 0; mi < model.Materials.Count; ++mi)
+			{
+				var mat = model.Materials[mi];
+				res += $"mat{mi} = bpy.data.materials.new('{mat.Name}')\n";
+
+				for (var ti = 0; ti < 3; ++ti)
+				{
+					if (!mat.EnabledTextures[ti]) continue;
+
+					string tex = null;
+					switch (ti)
+					{
+						case 0: tex = mat.Texture0Name; break;
+						case 1: tex = mat.Texture1Name; break;
+						case 2: tex = mat.Texture2Name; break;
+					}
+
+					if (tex == null) continue;
+
+					res += $"tex{mi}_{ti} = bpy.data.textures.new('{tex}', type='IMAGE')\n";
+					res += $"slt{mi}_{ti} = mat{mi}.texture_slots.add()\n";
+					res += $"slt{mi}_{ti}.texture = tex{mi}_{ti}\n";
+				}
+			}
+
+			return res;
+		}
+
 		private string BuildModel(H3DModel model)
 		{
-			if (model.Meshes.Count == 0)
-				return "";
-
-			var res =
-				$"root = bpy.data.objects.new('{model.Name}', None)\n" +
-				$"bpy.context.scene.objects.link(root)\n";
+			var res = "";
 
 			for (var mi = 0; mi < model.Meshes.Count; ++mi)
 			{
 				var mesh = model.Meshes[mi];
-				var vertices = MeshTransform.GetWorldSpaceVertices(model.Skeleton, mesh);
+
+				if (mesh.Type == H3DMeshType.Silhouette) continue;
+
+				var vertices = mesh.GetVertices();
+
+				res += $"m{mi} = bpy.data.meshes.new('{model.MeshNodesTree.Find(mesh.NodeIndex)}')\n";
+				res += $"o{mi} = bpy.data.objects.new('{model.MeshNodesTree.Find(mesh.NodeIndex)}', m{mi})\n";
+				res += $"o{mi}.parent = root\n";
+				res += $"bpy.context.scene.objects.link(o{mi})\n";
+				res += $"vs{mi} = []\n";
+				res += $"uv{mi} = []\n";
+				res += $"bm{mi} = bmesh.new()\n";
 
 				for (var smi = 0; smi < mesh.SubMeshes.Count; ++smi)
 				{
 					var subMesh = mesh.SubMeshes[smi];
-
-					res += $"m{mi}_{smi} = bpy.data.meshes.new('{model.MeshNodesTree.Find(mesh.NodeIndex)}_{mi}_{smi}')\n";
-					res += $"o{mi}_{smi} = bpy.data.objects.new('{model.MeshNodesTree.Find(mesh.NodeIndex)}_{mi}_{smi}', m{mi}_{smi})\n";
-					res += $"o{mi}_{smi}.parent = root\n";
-					res += $"bpy.context.scene.objects.link(o{mi}_{smi})\n";
-
-					res += $"bm{mi}_{smi} = bmesh.new()\n";
+					var tris = subMesh.Indices;
 
 					// Y and Z are swapped in blender's space
-					foreach (var vert in vertices)
-						res += $"bm{mi}_{smi}.verts.new([{vert.Position.X * SCALE},{-vert.Position.Z * SCALE},{vert.Position.Y * SCALE}])\n";
+					for (var vi = 0; vi < vertices.Length; ++vi)
+					{
+						var vert = vertices[vi];
+					
+						res += $"v{vi} = bm{mi}.verts.new([{vert.Position.X * SCALE},{-vert.Position.Z * SCALE},{vert.Position.Y * SCALE}])\n";
+						res += $"v{vi}.normal = [{vert.Normal.X},{vert.Normal.Z},{vert.Normal.Y}]\n";
+						res += $"vs{mi}.append(v{vi})\n";
+						res += $"uv{mi}.append(({vert.TexCoord0.X},{vert.TexCoord0.Y}))\n";
+					}
 
-					res += $"bm{mi}_{smi}.to_mesh(m{mi}_{smi})\n";
-					res += $"bm{mi}_{smi}.free()\n";
+					// Blender does not like duplicate tris, so we need to add an additional check
+					var placedTris = new List<ushort[]>();
+					for (ushort i = 0; i < tris.Length; i += 3)
+					{
+						var tri = new ushort[] { tris[i], tris[i + 1], tris[i + 2] };
+
+						if (placedTris.Any(x => x.SequenceEqual(tri)))
+							continue;
+
+						res += $"bm{mi}.faces.new([vs{mi}[{tri[0]}],vs{mi}[{tri[1]}],vs{mi}[{tri[2]}]]).smooth = True\n";
+						placedTris.Add(tri);
+					}
 				}
+
+				res += $"bm{mi}.verts.index_update()\n";
+				res += $"uvl{mi} = bm{mi}.loops.layers.uv.new()\n";
+				res += $"for face in bm{mi}.faces:\n\tfor loop in face.loops:\n\t\t";
+				res += $"loop[uvl{mi}].uv = uv{mi}[loop.vert.index]\n";
+
+				res += $"bm{mi}.to_mesh(m{mi})\n";
+				res += $"bm{mi}.free()\n";
+				res += $"o{mi}.data.materials.append(mat{mesh.MaterialIndex})\n";
+
+				// Sometimes the normals get messed up when entered manually, so we toggle editmode to make them right again
+				// It should not work but somehow it does
+				res += $"bpy.context.scene.objects.active = o{mi}\n";
+				res += $"o{mi}.select = True\n";
+				res += $"bpy.ops.object.editmode_toggle()\n";
+				res += $"bpy.ops.object.editmode_toggle()\n";
+				res += $"bpy.ops.object.select_all(action='DESELECT')\n";
 			}
 
 			return res;
