96810279048e559685ed07d2aa2f2b0e5ccafdd5
Akela <akela@null.net>
2019-03-31 11:02:04 +0200

    Optimization

--- SPICA/Formats/Generic/Blender/BLEND.cs
+++ SPICA/Formats/Generic/Blender/BLEND.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Text;
 
 namespace SPICA.Formats.Generic.Blender
 {
@@ -16,7 +17,7 @@ namespace SPICA.Formats.Generic.Blender
 		const float SCALE = 0.01f;
 
 		private readonly string blenderPath;
-		private string pythonScript;
+		private StringBuilder pythonScript;
 
 		public BLEND(H3D scene, int mdlIndex, int animIndex = -1)
 		{
@@ -40,16 +41,16 @@ namespace SPICA.Formats.Generic.Blender
 			{
 				var model = scene.Models[mdlIndex];
 
-				pythonScript = CleanUpScene(model);
+				CleanUpScene(model);
 
 				if (model.Materials.Count > 0)
-					pythonScript += BuildMaterials(model);
+					BuildMaterials(model);
 
 				if (model.Skeleton.Count > 0)
-					pythonScript += BuildArmature(model);
+					BuildArmature(model);
 
 				if (model.Meshes.Count > 0)
-					pythonScript += BuildModel(model);
+					BuildModel(model);
 			}
 		}
 
@@ -58,7 +59,7 @@ namespace SPICA.Formats.Generic.Blender
 			if (!File.Exists(blenderPath))
 				throw new FileNotFoundException("Blender not found");
 
-			pythonScript += $"bpy.ops.wm.save_as_mainfile(filepath='{fileName}')\n";
+			pythonScript.Append($"bpy.ops.wm.save_as_mainfile(filepath='{fileName}')\n");
 
 			var scriptPath = Path.Combine(Path.GetTempPath(), "export_" + Path.GetFileNameWithoutExtension(fileName) + ".py");
 			using (StreamWriter sw = new StreamWriter(scriptPath))
@@ -76,31 +77,29 @@ namespace SPICA.Formats.Generic.Blender
 		}
 
 		#region Private Methods
-		private string CleanUpScene(H3DModel model)
+		private void CleanUpScene(H3DModel model)
 		{
 			var armature = model.Skeleton.Count == 0 ? "None" : $"bpy.data.armatures.new('{model.Skeleton[0].Name}')";
 			var modelName = model.Skeleton.Count == 0 ? model.Name : model.Skeleton[0].Name;
 
-			return
-				"import bpy\n" +
-				"import bmesh\n" +
-				"import math\n" +
-				"import mathutils\n" +
-				"bpy.context.scene.render.engine = 'BLENDER_RENDER'\n" +
-				"bpy.ops.object.select_all()\n" +
-				"bpy.ops.object.delete()\n" +
-				$"root = bpy.data.objects.new('{modelName}', {armature})\n" +
-				"bpy.context.scene.objects.link(root)\n";
+			pythonScript = new StringBuilder(
+$@"import bpy
+import bmesh
+bpy.context.scene.render.engine = 'BLENDER_RENDER'
+bpy.ops.object.select_all()
+bpy.ops.object.delete()
+root = bpy.data.objects.new('{modelName}', {armature})
+bpy.context.scene.objects.link(root)
+"
+			);
 		}
 
-		private string BuildMaterials(H3DModel model)
+		private void BuildMaterials(H3DModel model)
 		{
-			var res = "";
-
 			for (var mi = 0; mi < model.Materials.Count; ++mi)
 			{
 				var mat = model.Materials[mi];
-				res += $"mat{mi} = bpy.data.materials.new('{mat.Name}')\n";
+				pythonScript.Append($"mat{mi} = bpy.data.materials.new('{mat.Name}')\n");
 
 				for (var ti = 0; ti < 3; ++ti)
 				{
@@ -116,27 +115,25 @@ namespace SPICA.Formats.Generic.Blender
 
 					if (tex == null) continue;
 
-					res += $"tex{mi}_{ti} = bpy.data.textures.new('{tex}', type='IMAGE')\n";
-					res += $"slt{mi}_{ti} = mat{mi}.texture_slots.add()\n";
-					res += $"slt{mi}_{ti}.texture = tex{mi}_{ti}\n";
+					pythonScript.Append($"tex{mi}_{ti} = bpy.data.textures.new('{tex}', type='IMAGE')\n");
+					pythonScript.Append($"slt{mi}_{ti} = mat{mi}.texture_slots.add()\n");
+					pythonScript.Append($"slt{mi}_{ti}.texture = tex{mi}_{ti}\n");
 				}
 			}
-
-			return res;
 		}
 
-		private string BuildArmature(H3DModel model)
+		private void BuildArmature(H3DModel model)
 		{
-			var res = "bpy.context.scene.objects.active = root\n";
-			res += "bpy.ops.object.editmode_toggle()\n";
+			pythonScript.Append("bpy.context.scene.objects.active = root\n");
+			pythonScript.Append("bpy.ops.object.editmode_toggle()\n");
 
 			// First bone is the armature
 			for (var bi = 1; bi < model.Skeleton.Count; ++bi)
 			{
 				var bone = model.Skeleton[bi];
 
-				res += $"b{bi} = root.data.edit_bones.new('{bone.Name}')\n";
-				res += $"b{bi}.tail = (0,0,1)\n";
+				pythonScript.Append($"b{bi} = root.data.edit_bones.new('{bone.Name}')\n");
+				pythonScript.Append($"b{bi}.tail = (0,0,1)\n");
 			}
 
 			// We have to do it a second time to set the parents
@@ -146,10 +143,10 @@ namespace SPICA.Formats.Generic.Blender
 
 				if (bone.ParentIndex == 0) continue;
 
-				res += $"b{bi}.parent = b{bone.ParentIndex}\n";
+				pythonScript.Append($"b{bi}.parent = b{bone.ParentIndex}\n");
 			}
 
-			res += "bpy.ops.object.editmode_toggle()\n";
+			pythonScript.Append("bpy.ops.object.editmode_toggle()\n");
 
 			// And a third time for the pose bones, which are different entities in blender
 			for (var bi = 1; bi < model.Skeleton.Count; ++bi)
@@ -157,24 +154,21 @@ namespace SPICA.Formats.Generic.Blender
 				var bone = model.Skeleton[bi];
 				var t = bone.Transform;
 
-				res += $"b{bi} = root.pose.bones[{bi - 1}]\n";
-				res += $"b{bi}.matrix_basis = (({t.M11},{t.M12},{t.M13},{t.M14}),({t.M21},{t.M22},{t.M23},{t.M24}),({t.M31},{t.M32},{t.M33},{t.M34}),({t.M41},{t.M42},{t.M43},{t.M44}))\n";
+				pythonScript.Append($"b{bi} = root.pose.bones[{bi - 1}]\n");
+				pythonScript.Append($"b{bi}.matrix_basis = (({t.M11},{t.M12},{t.M13},{t.M14}),({t.M21},{t.M22},{t.M23},{t.M24}),({t.M31},{t.M32},{t.M33},{t.M34}),({t.M41},{t.M42},{t.M43},{t.M44}))\n");
 			}
 
-			res += "bpy.ops.object.select_all(action='DESELECT')\n";
-
-			return res;
+			pythonScript.Append("bpy.ops.object.select_all(action='DESELECT')\n");
 		}
 
-		private string BuildModel(H3DModel model)
+		private void BuildModel(H3DModel model)
 		{
-			var res = "";
-			res += $"m = bpy.data.meshes.new('{model.Name}')\n";
-			res += $"o = bpy.data.objects.new('{model.Name}', m)\n";
-			res += $"o.parent = root\n";
-			res += $"bpy.context.scene.objects.link(o)\n";
-			res += $"bm = bmesh.new()\n";
-			res += $"uvl = bm.loops.layers.uv.new()\n";
+			pythonScript.Append($"m = bpy.data.meshes.new('{model.Name}')\n");
+			pythonScript.Append($"o = bpy.data.objects.new('{model.Name}', m)\n");
+			pythonScript.Append($"o.parent = root\n");
+			pythonScript.Append($"bpy.context.scene.objects.link(o)\n");
+			pythonScript.Append($"bm = bmesh.new()\n");
+			pythonScript.Append($"uvl = bm.loops.layers.uv.new()\n");
 
 			var vertices = new List<BLENDVertex>();
 			var tris = new List<int[]>();
@@ -240,52 +234,50 @@ namespace SPICA.Formats.Generic.Blender
 				}
 			}
 
-			res += $"vs = []\n";
+			pythonScript.Append($"vs = []\n");
 
 			// Y and Z are swapped in blender's space
 			foreach (var v in vertices)
 			{
-				res += $"v = bm.verts.new([{v.vert.Position.X},{-v.vert.Position.Z},{v.vert.Position.Y}])\n";
-				res += $"v.normal = [{v.vert.Normal.X},{-v.vert.Normal.Z},{v.vert.Normal.Y}]\n";
-				res += $"vs.append(v)\n";
+				pythonScript.Append($"v = bm.verts.new([{v.vert.Position.X},{-v.vert.Position.Z},{v.vert.Position.Y}])\n");
+				pythonScript.Append($"v.normal = [{v.vert.Normal.X},{-v.vert.Normal.Z},{v.vert.Normal.Y}]\n");
+				pythonScript.Append($"vs.append(v)\n");
 			}
 
-			res += "bm.verts.index_update()\n";
+			pythonScript.Append("bm.verts.index_update()\n");
 
 			for (var ti = 0; ti < tris.Count; ++ti)
 			{
 				var tri = tris[ti];
 
-				res += $"f = bm.faces.new([vs[{tri[0]}],vs[{tri[1]}],vs[{tri[2]}]])\n";
-				res += "f.smooth = True\n";
-				res += "lv = {}\n";
+				pythonScript.Append($"f = bm.faces.new([vs[{tri[0]}],vs[{tri[1]}],vs[{tri[2]}]])\n");
+				pythonScript.Append("f.smooth = True\n");
+				pythonScript.Append("lv = {}\n");
 
 				foreach (var loop in loops.Where(x => x.face == ti))
-					res += $"lv['{loop.vert}'] = [{loop.uv.X},{loop.uv.Y}]\n";
+					pythonScript.Append($"lv['{loop.vert}'] = [{loop.uv.X},{loop.uv.Y}]\n");
 
-				res += $"for loop in f.loops: loop[uvl].uv = lv[str(loop.vert.index)]\n";
+				pythonScript.Append($"for loop in f.loops: loop[uvl].uv = lv[str(loop.vert.index)]\n");
 			}
 
 			foreach (var mat in materials)
-				res += $"o.data.materials.append(mat{mat})\n";
+				pythonScript.Append($"o.data.materials.append(mat{mat})\n");
 
-			res += $"bm.to_mesh(m)\n";
-			res += $"bm.free()\n";
-			res += $"bpy.context.scene.objects.active = o\n";
-			res += $"o.select = True\n";
-			res += $"bpy.ops.object.editmode_toggle()\n";
-			res += $"bpy.ops.object.editmode_toggle()\n";
-			res += $"bpy.ops.object.select_all(action='DESELECT')\n";
+			pythonScript.Append($"bm.to_mesh(m)\n");
+			pythonScript.Append($"bm.free()\n");
+			pythonScript.Append($"bpy.context.scene.objects.active = o\n");
+			pythonScript.Append($"o.select = True\n");
+			pythonScript.Append($"bpy.ops.object.editmode_toggle()\n");
+			pythonScript.Append($"bpy.ops.object.editmode_toggle()\n");
+			pythonScript.Append($"bpy.ops.object.select_all(action='DESELECT')\n");
 
 			for (var gi = 0; gi < groups.Count; ++gi)
 			{
 				var indexes = vertices.Where(x => x.vertGroup == gi).Select(x => vertices.IndexOf(x));
 			
-				res += $"vg = o.vertex_groups.new('{groups[gi]}')\n";
-				res += $"vg.add([{string.Join(",", indexes)}], 1, 'ADD')\n";
+				pythonScript.Append($"vg = o.vertex_groups.new('{groups[gi]}')\n");
+				pythonScript.Append($"vg.add([{string.Join(",", indexes)}], 1, 'ADD')\n");
 			}
-
-			return res;
 		}
 		#endregion
 	}
